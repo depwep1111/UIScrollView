c885fd1db3514b82a7eaa1a5830a1629fa049bf2
tran thanh <trantrungthanh@trans-Mac.local>
2017-05-06 00:19:46 +0700

    Initial Commit

---
+++ UIScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIScrollView
+//
+//  Created by tran trung thanh on 5/6/17.
+//  Copyright © 2017 tran trung thanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
